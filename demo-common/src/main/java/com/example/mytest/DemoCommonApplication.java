2d8d23e46d9381e3af87a5f26b4bd15b353cab14
xupengfei <1830166767@qq.com>
2018-12-04 20:30:04 +0800

    Initial commit

---
+++ demo-common/src/main/java/com/example/mytest/DemoCommonApplication.java
@@ -0,0 +1,11 @@
+package com.example.mytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class DemoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCommonApplication.class, args);
+    }
+}
---
+++ demo-common/src/test/java/com/example/mytest/DemoCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class DemoCommonApplicationTests {
+
+    @Test public void contextLoads() {
+    }
+
+}
